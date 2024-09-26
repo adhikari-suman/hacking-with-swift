6186460fef4faf77b30127436270f737d3edd860
Suman Adhikari <expertxkid@gmail.com>
2024-09-26 13:45:59 -0500

    init: init project-0002-GuessTheFlag

---
+++ 100-days-of-SwiftUI/Project-0002-GuessTheFlag/Project-0002-GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-0002-GuessTheFlag
+//
+//  Created by Suman Adhikari on 9/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello, world!")
+    }
+
+
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100-days-of-SwiftUI/Project-0002-GuessTheFlag/Project-0002-GuessTheFlag/Project_0002_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_0002_GuessTheFlagApp.swift
+//  Project-0002-GuessTheFlag
+//
+//  Created by Suman Adhikari on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_0002_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
