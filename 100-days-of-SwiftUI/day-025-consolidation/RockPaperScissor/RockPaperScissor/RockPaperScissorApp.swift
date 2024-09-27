559306c30258b0e4a2a06fe76835d53b95c3283c
Suman Adhikari <expertxkid@gmail.com>
2024-09-27 09:20:26 -0500

    feat: complete day-025-consolidation project

---
+++ 100-days-of-SwiftUI/day-025-consolidation/RockPaperScissor/RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Suman Adhikari on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
